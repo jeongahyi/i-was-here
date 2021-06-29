f487c691d4a242180f9a60c497a0910c5d9e1993
Jeongah Yi <jeongahyi@gmail.com>
2021-06-30 01:49:54 +0900

    Delete images component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Header from './components/Header';
 import Map from './components/Map';
-import Images from './components/Images';
+// import Images from './components/Images';
 import Footer from './components/Footer';
 
 const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    margin: 'auto',
+    margin: '0',
     minHeight: '100vh',
-    paddingLeft: '80px',
-    paddingRight: '80px',
+    backgroundColor: 'whitesmoke',
   },
   main: {
     alignSelf: 'center',
+    marginTop: '30px',
+    marginBottom: '100px',
   }
 }));
 
 const App = () => {
   const classes = useStyles();
-  const [pictures, setPictures] = useState({});
   return (
     <div className={classes.root}>
       <Header />
       <main className={classes.main}>
-        <Map setPictures={setPictures} />
-        <Images pictures={pictures} />
+        <Map />
+        {/* <Images /> */}
       </main>
       <Footer />
     </div>
